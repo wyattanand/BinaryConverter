fcd45a548d3edf954e19f094442aeefd020244ad
wyatt <wanand1@hwemail.com>
2023-03-14 08:33:40 -0700

    First Draft
    Still some bugs with biToDec and taking in a number greater than 100,000,000,000 in decToBi

---
+++ src/BinaryConverter/Converter.java
@@ -0,0 +1,28 @@
+package BinaryConverter;
+
+public class Converter {
+	public static String decToBi(long dec) {
+		String binary = "";
+		while (dec > 0) {
+			long remainder = dec % 2;
+			binary = remainder + binary;
+			dec = dec / 2;
+		}
+		return binary;
+	}
+	public static String biToDec(String binary) {
+		long decimal = 0;
+		int j = 0;
+		for (int i = binary.length()-1; i >= 0; i--) {
+			decimal += (long)(((int)(binary.charAt(j))) * Math.pow(2, i));
+			j++;
+		}
+		return decimal+"";
+	}
+	public static void main(String [] args) {
+		System.out.println(decToBi(1000000000));
+		System.out.println(biToDec("111011100110101100101000000000"));
+	}
+}
+
+
